60ba29c303c146d18beb48820338a52d42926860
Blue Pigeons <onlysky@hotmail.co.uk>
2016-09-14 11:21:21 +0100

    final commit from mac

--- polyanno_storage.js
+++
@@ -1,8 +0,0 @@
-
-exports.setup = require('src/routes_setup');
-exports.annotations = require('src/annotations');
-exports.vectors = require('src/vectors');
-exports.transcriptions = require('src/transcriptions');
-exports.translations = require('src/translations');
-exports.users = require('src/usersroute');
-
